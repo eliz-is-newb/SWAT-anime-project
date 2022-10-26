d7b2e1873037f679056c423ff7814359b51ae182
Sky DeGrate <skydegrate@gmail.com>
2022-10-26 11:17:58 -0400

    Yes, progress :)

---
+++ client/src/views/Library.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import CoreBar from "../components/CoreBar";
+import CoreFooter from "../components/CoreFooter";
+import Map from "../components/Library/Map";
+
+function Library(){
+
+
+    return (
+        <>
+            <CoreBar />
+            
+            <Map />
+            <CoreFooter />
+        </>
+    );
+}
+
+export default Library;
