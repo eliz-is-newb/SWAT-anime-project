d130167945c4750e209ae12adc9657f2fc8320a0
Sky DeGrate <skydegrate@gmail.com>
2023-03-29 14:51:53 -0400

    fixed: some things

--- client/src/components/Library/AnimeStreamPlayer.js
+++ client/src/components/Library/AnimeStreamPlayer.js
@@ -26,12 +26,12 @@ function AnimeStreamPlayer({videoData}){
     // console.log(videoData)
     return (
         <div className="mb-10">
-         <h1  className="text-4xl bg-white text-black font-bold mt-10 ml-56 -mb-12 mx-10 px-10"> 
-           Watching: {videoData1?.anime?.anime_title} </h1>
+         <h1  className=" bg-white text-black mt-10 ml-10 -mb-12 mx-10 px-10"> 
+          <span className="bg-white text-2xl text-black font-bold px-2 py-3 rounded">You're watching:</span>  <span className="bg-black text-white px-5 font-bold text-xl py-3 rounded">{videoData1?.anime?.anime_title} </span></h1>
             {episodes.map((item)=>{
                 return (
                     
-            <div className="mt-30 bg-white flex" style={{ marginRight:"-400px", }}>
+            <div className="mt-30 bg-white flex -ml-40">
 
             
          
@@ -44,7 +44,7 @@ function AnimeStreamPlayer({videoData}){
             </div> 
         
             <div className="-ml-72" style={{position:"relative",zIndex:"-0", marginTop:"605px", marginLeft:"160px"}}>
-            <a href="#item" className="text-2xl  bg-lime-500 text-black"> &nbsp;Episode {item.ep_num}&nbsp; </a> 
+            <a href="#item" className="text-xl font-bold bg-black px-2 rounded py-2 text-white"> &nbsp;Episode {item.ep_num}&nbsp; </a> 
             </div>
             </div>
  )})}
